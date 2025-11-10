bdb2214e81aeaf9af8d4fae72b54292542a9ab25
Justin Casey <justin.c.casey@gmail.com>
2025-11-10 17:04:18 -0500

    Update to version 1.10.25 - Fix ZIP structure for WordPress updates

--- aqm-formidable-zip-whitelist.php
+++ aqm-formidable-zip-whitelist.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AQM Formidable ZIP & State Whitelist (Hardened)
  * Description: Server-side ZIP/State allowlist for Formidable Forms. Auto-detects ZIP/State fields; error color/size controls. Hardened against Unicode/invisible chars and double-enforced on create/update.
- * Version: 1.10.24
+ * Version: 1.10.25
  * Author: AQ Marketing (Justin Casey)
  * License: GPL-2.0+
  */
@@ -15,7 +15,7 @@ if (!class_exists('AQM_Formidable_Location_Whitelist')) {
 class AQM_Formidable_Location_Whitelist {
     const OPTION    = 'aqm_ff_location_whitelist';
     const PAGE_SLUG = 'aqm-ff-location-whitelist';
-    const VERSION   = '1.10.24';
+    const VERSION   = '1.10.25';
     private static $script_added = false;
 
     public function __construct() {
